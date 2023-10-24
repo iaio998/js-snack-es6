d95f22a5f6091418caf3cfb5b6b90c63a4edc0dd
iaio998 <depalatisiacopo@gmail.com>
2023-10-24 16:01:05 +0200

    Added array students

--- javascript/script.js
+++ javascript/script.js
@@ -23,10 +23,50 @@ vipTable.forEach((item, index) => {
   const vip = {
     tableName: "Vip Table",
     guestName: item,
-    place: index,
+    place: index + 1,
   };
   newTable.push(vip);
 });
 console.log(newTable);
 
 // SNACK 2
+const snack2 = "SNACK 2";
+console.log(snack2);
+
+const students = [
+  {
+    id: 213,
+    name: "Marco della Rovere",
+    grades: 78,
+  },
+  {
+    id: 110,
+    name: "Paola Cortellessa",
+    grades: 96,
+  },
+  {
+    id: 250,
+    name: "Andrea Mantegna",
+    grades: 48,
+  },
+  {
+    id: 145,
+    name: "Gaia Borromini",
+    grades: 74,
+  },
+  {
+    id: 196,
+    name: "Luigi Grimaldello",
+    grades: 68,
+  },
+  {
+    id: 102,
+    name: "Piero della Francesca",
+    grades: 50,
+  },
+  {
+    id: 120,
+    name: "Francesca da Polenta",
+    grades: 84,
+  },
+];
